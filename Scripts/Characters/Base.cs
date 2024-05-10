877bb47dc8180d407954abb8e546a3dc1d3d8f0b
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-05-11 02:52:47 +0300

    Changed the camera transfom value a bit and a label to display the base health and enabled the enemies from reducing said health

--- Scripts/Characters/Base.cs
+++ Scripts/Characters/Base.cs
@@ -3,8 +3,41 @@ using System;
 
 public partial class Base : Node3D
 {
+    [ExportGroup("Child Nodes")]
+    [Export]
+    public Label3D BaseLabel { get; private set; }
+
+    [ExportGroup("")]
+
+    [ExportCategory("Base Stats")]
+    [Export]
+    private int _maxHealth = 5;
+
+    
+    private int _currentHealth;
+
+
+
+    // Game Loop Methods---------------------------------------------------------------------------
+
+    public override void _Ready()
+    {
+        _currentHealth = _maxHealth;
+        UpdateBaseLabel();
+    }
+
+    // Member Methods------------------------------------------------------------------------------
+    
+    /// <summary>
+    /// Decrease the base health by one and update its label
+    /// </summary>
     public void TakeDamage()
     {
         GD.Print("I'm damaged :(");
+        _currentHealth--;
+
+        UpdateBaseLabel();
     }
+
+    private void UpdateBaseLabel() => BaseLabel.Text = $"{_currentHealth}";
 }
